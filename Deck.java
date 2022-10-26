16b8f2d94b87bf71df18f2eb89aba031ce9be736
JSiebor1 <siebjp@farmingdale.edu>
2022-10-26 13:49:34 -0400

    Changes to be committed: 	modified:   Deck.java

--- Deck.java
+++ Deck.java
@@ -54,46 +54,57 @@ public class Deck {
          * king=10 (13)
          */
 
-        // assigns names based on values
         for (int i = 0; i < deckSize; i++) {
-            if (deck[i].getValue() == 1)
-                deck[i].setName("Ace");
-            else if (deck[i].getValue() == 2)
-                deck[i].setName("Two");
-            else if (deck[i].getValue() == 3)
-                deck[i].setName("Three");
-            else if (deck[i].getValue() == 4)
-                deck[i].setName("Four");
-            else if (deck[i].getValue() == 5)
-                deck[i].setName("Five");
-            else if (deck[i].getValue() == 6)
-                deck[i].setName("Six");
-            else if (deck[i].getValue() == 7)
-                deck[i].setName("Seven");
-            else if (deck[i].getValue() == 8)
-                deck[i].setName("Eight");
-            else if (deck[i].getValue() == 9)
-                deck[i].setName("Nine");
-            else if (deck[i].getValue() == 10)
-                deck[i].setName("Ten");
-            else if (deck[i].getValue() == 11)
-                deck[i].setName("Jack");
-            else if (deck[i].getValue() == 12)
-                deck[i].setName("Queen");
-            else if (deck[i].getValue() == 13)
-                deck[i].setName("King");
+            switch (deck[i].getValue()) {
+                case 1:
+                    deck[i].setName("Ace");
+                    break;
+                case 2:
+                    deck[i].setName("Two");
+                    break;
+                case 3:
+                    deck[i].setName("Three");
+                    break;
+                case 4:
+                    deck[i].setName("Four");
+                    break;
+                case 5:
+                    deck[i].setName("Five");
+                    break;
+                case 6:
+                    deck[i].setName("Six");
+                    break;
+                case 7:
+                    deck[i].setName("Seven");
+                    break;
+                case 8:
+                    deck[i].setName("Eight");
+                    break;
+                case 9:
+                    deck[i].setName("Nine");
+                    break;
+                case 10:
+                    deck[i].setName("Ten");
+                    break;
+                case 11:
+                    deck[i].setName("Jack");
+                    break;
+                case 12:
+                    deck[i].setName("Queen");
+                    break;
+                case 13:
+                    deck[i].setName("King");
+                    break;
+            }
         }
 
         // set courts to 10
-        for(int i = 0; i < deckSize; i++) {
-            if(deck[i].getValue() > 10)
+        for (int i = 0; i < deckSize; i++) {
+            if (deck[i].getValue() > 10)
                 deck[i].setValue(10);
         }
 
         setDeck(shuffle(deck));
-
-        for (int i = 0; i < deckSize; i++)
-            System.out.println(deck[i].toString());
     }
 
     public Card[] shuffle(Card[] deck) {
@@ -105,7 +116,6 @@ public class Deck {
             temp = deck[x];
             deck[x] = deck[y];
             deck[y] = temp;
-
         }
 
         return deck;

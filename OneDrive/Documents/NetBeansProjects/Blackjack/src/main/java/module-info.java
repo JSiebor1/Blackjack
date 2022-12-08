1f98bb94b216a7888cfc4ab0bebd591abc77dffc
JSiebor1 <116303417+JSiebor1@users.noreply.github.com>
2022-12-08 14:38:38 -0500

    Delete OneDrive/Documents/NetBeansProjects directory

--- OneDrive/Documents/NetBeansProjects/Blackjack/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.blackjackapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.blackjackapp to javafx.fxml;
-    exports com.mycompany.blackjackapp;
-}
